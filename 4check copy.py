98b3ca176e18d272fbe661ab7b08ccca95d7261e
efecini <efecini@gmail.com>
2024-05-09 04:05:55 +0300

    the end

--- 4check copy.py
+++
@@ -1,8 +0,0 @@
-from util import readFromTXTFileAndGenerateArray
-
-v23_array_ = readFromTXTFileAndGenerateArray('/Users/efecini-plentific/Desktop/bip-0039:turkish.txt/do_not_change/final_v23.txt')
-print('--------------------START')
-for item in v23_array_:
-  if len(item) < 3 or len(item) > 8 :
-    print(item)
-print('--------------------END')
